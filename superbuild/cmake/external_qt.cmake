16727b2fa6e8f0fe734f5925a366bd654082cd73
Deepankar Sharma <deepankarsharma@gmail.com>
2024-02-12 21:36:59 -0500

    Restrict scope of qt modules being built

--- superbuild/cmake/external_qt.cmake
+++ superbuild/cmake/external_qt.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(qt
   GIT_REPOSITORY "https://github.com/qt/qt5"
   GIT_TAG        "e6ff9950e1d0365d2c4f231fd1b6fe194144fd52"  # v6.6.1
-  CONFIGURE_COMMAND perl <SOURCE_DIR>/init-repository && <SOURCE_DIR>/configure -prefix ${SB_INSTALL_PREFIX} -cmake-generator Ninja -static -release -nomake examples -nomake tests -qt-libpng -qt-zlib -qt-libjpeg -qt-freetype -qt-pcre -qt-harfbuzz -platform macx-clang -sql-sqlite -qt-sqlite
+  CONFIGURE_COMMAND perl <SOURCE_DIR>/init-repository -module-subset=qtbase,qtsvg,qtimageformats && <SOURCE_DIR>/configure -prefix ${SB_INSTALL_PREFIX} -cmake-generator Ninja -static -release -nomake examples -nomake tests -qt-libpng -qt-zlib -qt-libjpeg -qt-freetype -qt-pcre -qt-harfbuzz -sql-sqlite -qt-sqlite -no-zstd
   BUILD_COMMAND cmake --build . --parallel
   INSTALL_COMMAND cmake --install .
 )
