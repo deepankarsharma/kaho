70c7bace41ce9f1301f4a916ac17175ad2f70e8a
Deepankar Sharma <deepankarsharma@gmail.com>
2024-02-21 02:03:42 -0500

    Explicitly switch ci_build.sh to Release builds by default

--- bin/ci_build.sh
+++ bin/ci_build.sh
@@ -2,8 +2,19 @@
 
 set -eux
 
+# Set default build type to Release
+BUILD_TYPE=Release
+
+# Check for -debug argument
+for arg in "$@"; do
+    if [[ $arg == "-debug" ]]; then
+        BUILD_TYPE=Debug
+        break
+    fi
+done
+
 mkdir build
 pushd build
-    cmake ..
-    cmake --build . --parallel -- -j8
+    cmake -DCMAKE_BUILD_TYPE=${BUILD_TYPE} ..
+    cmake --build . --parallel
 popd
