48662e3b5e20b07121393073fbd5021a4fa8f1d1
Deepankar Sharma <deepankarsharma@gmail.com>
2024-03-01 21:25:58 -0500

    Added install step to qt

--- superbuild/cmake/external_qt.cmake
+++ superbuild/cmake/external_qt.cmake
@@ -4,13 +4,18 @@ else()
     set(BUILD_TYPE "-release")
 endif()
 
+set(QPA_FLAG "")
+if(LINUX)
+    set(QPA_FLAG "-qpa \"wayland,xcb\"")
+endif()
+
 ExternalProject_Add(qt
   GIT_REPOSITORY "https://github.com/qt/qt5"
   GIT_TAG        "e6ff9950e1d0365d2c4f231fd1b6fe194144fd52"  # v6.6.1
   GIT_SHALLOW    ON
   GIT_SUBMODULES ""
   BUILD_IN_SOURCE ON
-  CONFIGURE_COMMAND perl <SOURCE_DIR>/init-repository -f -module-subset=qtbase,qtsvg,qtimageformats && <SOURCE_DIR>/configure -static -prefix ${SB_INSTALL_PREFIX} -cmake-generator Ninja ${BUILD_TYPE} -nomake examples -nomake tests -qt-libpng -qt-zlib -qt-libjpeg -qt-freetype -qt-pcre -qt-harfbuzz -sql-sqlite -qt-sqlite -no-zstd
+  CONFIGURE_COMMAND perl <SOURCE_DIR>/init-repository -f -module-subset=qtbase,qtsvg,qtimageformats && <SOURCE_DIR>/configure -static -prefix ${SB_INSTALL_PREFIX} -cmake-generator Ninja ${BUILD_TYPE} -nomake examples -nomake tests -qt-libpng -qt-zlib -qt-libjpeg -qt-freetype -qt-pcre -qt-harfbuzz -sql-sqlite -qt-sqlite -no-zstd -qpa wayland,xcb
   BUILD_COMMAND cmake --build . --parallel -- -j8
   INSTALL_COMMAND cmake --install .
 )
